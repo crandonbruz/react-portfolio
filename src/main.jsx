2dc553534309cea939b3caa674f172af0ccb0e4b
crandonbruz <bcruz12127@gmail.com>
2024-11-14 11:18:52 -0500

    fixed the routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,38 @@
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-import Error from "./pages/Error/Error";
-import About from "./pages/About/About";
-import Portfolio from "./pages/Portfolio/Portfolio";
-import Contact from "./pages/Contact/Contact";
-import Resume from "./pages/Resume/Resume";
+// import Error from "./pages/Error/Error";
+// import About from "./pages/About/About";
+// import Portfolio from "./pages/Portfolio/Portfolio";
+// import Contact from "./pages/Contact/Contact";
+// import Resume from "./pages/Resume/Resume";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/portfolio",
-        element: <Portfolio />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/resume",
-        element: <Resume />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <Error />,
+//     children: [
+//       {
+//         path: "/about",
+//         element: <About />,
+//       },
+//       {
+//         path: "/portfolio",
+//         element: <Portfolio />,
+//       },
+//       {
+//         path: "/contact",
+//         element: <Contact />,
+//       },
+//       {
+//         path: "/resume",
+//         element: <Resume />,
+//       },
+//     ],
+//   },
+// ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
